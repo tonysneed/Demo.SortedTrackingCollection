a7d4052e4da591523bd50664e3ccc538c4aca7d9
Anthony Sneed <tony@tonysneed.com>
2016-01-06 18:07:41 +0100

    Added VS2015 solution with unit test to sort a ChangeTrackingCollection with dynamic linq.

---
+++ Demo.SortedTrackingCollection/ChangeTrackingCollectionTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Linq.Dynamic;
+using TrackableEntities.Client;
+using Xunit;
+
+namespace Demo.SortedTrackingCollection
+{
+    public class ChangeTrackingCollectionTests
+    {
+        [Fact]
+        public void Can_sort_ChangeTrackingCollection_using_dynamic_linq()
+        {
+            // Arrange
+            var product1 = new Product {Id = 1, ProductName = "Espresso"};
+            var product2 = new Product { Id = 1, ProductName = "Chai" };
+            var changeTracker = new ChangeTrackingCollection<Product>(product1, product2);
+            var queryable = changeTracker.AsQueryable().OrderBy("ProductName asc");
+
+            // Act
+            var sortedEntities = queryable.ToList();
+
+            // Assert
+            Assert.Equal("Chai", sortedEntities.ElementAt(0).ProductName);
+            Assert.Equal("Espresso", sortedEntities.ElementAt(1).ProductName);
+        }
+    }
+}
---
+++ Demo.SortedTrackingCollection/Product.cs
@@ -0,0 +1,32 @@
+﻿using TrackableEntities.Client;
+
+namespace Demo.SortedTrackingCollection
+{
+    public class Product : EntityBase
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _productName;
+
+        public string ProductName
+        {
+            get { return _productName; }
+            set
+            {
+                _productName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+    }
+}
